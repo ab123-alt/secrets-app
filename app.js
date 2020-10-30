f15d46d27129e85b8bf9a7c2049c62760c5ee56c
busa <agnes.busa@adesso.de>
2020-10-30 08:09:17 +0100

    init barebones project

--- app.js
+++ app.js
@@ -1,1 +1,38 @@
 //jshint esversion:6
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const _ = require("lodash");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// DB
+
+mongoose.connect("mongodb://localhost:27017/todolistDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// http
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+app.listen(3000, () => {
+  console.log("server started on port 3000");
+});
